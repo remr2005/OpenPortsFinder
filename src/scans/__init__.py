eee61ad2ec9e38a68277375e494f5955cbace972
UltraUberTurk <morpeh2005ka@gmail.com>
2025-05-24 13:58:10 +0300

    Вроде закончил все основные сканы

--- src/scans/__init__.py
+++ src/scans/__init__.py
@@ -2,6 +2,11 @@
 
 from .ack_scan import ack
 from .fin_scan import fin
+from .find_cve import scan_network
+from .icmp_scan import icmp_address_mask_scan, icmp_echo_scan, icmp_timestamp_scan
+from .idle_scan import idle
 from .null_scan import null
+from .special_scan import fragmented_tcp_scan, source_port_manipulation
 from .syn_scan import syn
+from .udp_scan import udp
 from .xmas_scan import xmas
